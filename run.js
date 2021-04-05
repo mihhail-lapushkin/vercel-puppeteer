2d9a632eabe63ee6e7bee35657368ab8464bad79
mihhail-lapushkin <mihhail.lapushkin@gmail.com>
2021-04-05 19:51:05 +0300

    initial commit

---
+++ run.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+(async () => {
+  await fs.promises.mkdir('public', { recursive: true });
+  await fs.promises.writeFile('public/index.html', '<img src="/image.png">');
+
+  const browser = await puppeteer.launch(process.env.AWS_REGION ? {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  } : {
+    args: [],
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  });
+
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 400,
+    height: 400,
+    deviceScaleFactor: 1
+  });
+
+  await page.setContent('<h1>Hello World!</h1>', { waitUntil: 'networkidle2' });
+  await page.screenshot({ path: 'public/image.png' });
+  await browser.close();
+})();
